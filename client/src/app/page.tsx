18b2375391a6983cea79fdc4b4684c187d4c7b42
Aruminium <b2201680@photon.chitose.ac.jp>
2023-08-19 11:24:49 +0900

    first commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-12 gap-4">
+      <main className="col-start-3 col-end-11"><h1>aaa</h1></main>
+    </div>
+  );
+}
